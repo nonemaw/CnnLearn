4f5b3c8d7fa0c9d56c28d951d4c995b8c08b4e15
Chengjia Xu <chexu@microsoft.com>
2022-03-05 09:54:56 +0800

    ?

--- conv.py
+++ conv.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numpy.typing import *
 
+
 class Conv3x3:
     """
     https://victorzhou.com/blog/intro-to-cnns-part-1/
@@ -19,11 +20,14 @@ class Conv3x3:
         # Initialize a random filter matrix with num_filters x 3 x 3 dimensions
         # - It contains "num_filters" of 3x3 filters
         #
+        # np.random.rand(num_filters, 3, 3) 从标准分布中初始化出"num_filters"个3x3方阵作为过滤器
+        #
         # Divided by 9 to reduce the variance of our initial values
         # - Diving by 9 during the initialization is more important than you may think. If the initial values are
         #   too large or too small, training the network will be ineffective.
         # - To learn more, read about: https://www.quora.com/What-is-an-intuitive-explanation-of-the-Xavier-Initialization-for-Deep-Neural-Networks
         self.filters: NDArray = np.random.randn(num_filters, 3, 3) / 9
+        print(self.filters)
 
     def iterate_regions(self, image: NDArray) -> (NDArray, int, int):
         """
@@ -35,7 +39,7 @@ class Conv3x3:
         h, w = image.shape
         for i in range(h - 2):
             for j in range(w - 2):
-                im_region: NDArray = image[i:(i+3), j:(j+3)]
+                im_region: NDArray = image[i:(i + 3), j:(j + 3)]
                 yield im_region, i, j
 
     def forward(self, padded_image: NDArray) -> NDArray:
@@ -48,11 +52,13 @@ class Conv3x3:
         Returns a 3d numpy array with dimensions (h, w, num_filters).
         """
         h, w = padded_image.shape
-        output: NDArray = np.zeros((h-2, w-2, self.num_filters))
+        output: NDArray = np.zeros((h - 2, w - 2, self.num_filters))
 
         for region, i, j in self.iterate_regions(padded_image):
             # Calculate current image region with filter matrix (num_filters of 3x3 filters) and get result
+            #
+            # np.sum() calculates the result of the previous step using axis=(1, 2), which produces a 1d array
+            # of length "num_filters" where each element contains the convolution result for the corresponding filter
             output[i, j] = np.sum(region * self.filters, axis=(1, 2))
 
         return output
-
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import mnist
+
 from conv import Conv3x3
 
 # The mnist package handles the MNIST dataset for us!
@@ -6,6 +7,14 @@ from conv import Conv3x3
 train_images = mnist.train_images()
 train_labels = mnist.train_labels()
 
-conv = Conv3x3(8)
+conv = Conv3x3(num_filters=8)
 output = conv.forward(train_images[0])
-print(output.shape) # (26, 26, 8)
+print(output.shape)  # (26, 26, 8)
+
+"""
+在这个3x3卷积中，我们已经预先假设了input是一个二维NDArray，这是因为一个MNIST图像就是以
+这种方式存储的
+
+当它作为卷积的第一层时这是能够工作的，但是在真实环境中CNN会有很多不同的卷积层。如果我们需
+要建立一个使用多个3x3卷积过滤的CNN网络，那么我们必须提供一个三维的NDArray作为input
+"""
